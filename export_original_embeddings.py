273528d4964a2c1729d5c1b64c32f1dc2c910062
Mehdi Azabou <mehdiazabou@gmail.com>
2022-11-19 17:51:58 -0500

    fix undefined variable error

--- export_original_embeddings.py
+++ export_original_embeddings.py
@@ -12,7 +12,7 @@ def export_embed(lang):
         model_url = "WillHeld/es-bert-xnli"
     model = AutoModelForSequenceClassification.from_pretrained(model_url)
     embeddings = model.base_model.embeddings.word_embeddings.weight
-    embeddings.detach().numpy()
+    out_np = embeddings.detach().numpy()
     out_df = pd.DataFrame(out_np)
     out_df.to_csv(f"embedding_files/{lang}_embeddings.txt")
 
